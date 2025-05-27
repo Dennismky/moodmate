1f71bb86df17170682b66e47e0d82deed29c6662
tinamanga <tinamanga@gmail.com>
2025-05-27 16:56:21 +0300

    created crud .py file that it will work for creating ,reading,updating and deleting a moodlog as it is connected to the database

---
+++ lib/db/crud.py
@@ -0,0 +1,61 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import User, MoodLog
+from datetime import datetime
+
+
+# Connecting to the database
+db_path = 'lib/db/moodmate.db'
+engine = create_engine(f'sqlite:///lib/db/moodmate.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+# CREATE
+
+def create_mood_log(user_name, mood):
+    user = session.query(User).filter_by(name=user_name).first()
+    if not user:
+        print(f"User '{user_name}' not found.")
+        return
+    new_log = MoodLog(mood=mood, timestamp=datetime.now(), user_id=user.id)
+    session.add(new_log)
+    session.commit()
+    print(f"Mood '{mood}' added for user '{user_name}'")
+
+# READ
+
+def read_mood_logs(user_name=None):
+    if user_name:
+        logs = session.query(MoodLog).join(User).filter(User.name == user_name).all()
+    else:
+        logs = session.query(MoodLog).all()
+    for log in logs:
+        user = session.query(User).filter_by(id=log.user_id).first()
+        print(f"{user.name}: {log.mood} at {log.timestamp}")
+
+
+# UPDATE
+
+def update_mood_log(log_id, new_mood):
+    log = session.query(MoodLog).filter_by(id=log_id).first()
+    if not log:
+        print(f"Mood log with ID {log_id} not found.")
+        return
+    log.mood = new_mood
+    session.commit()
+    print(f"Updated mood log ID {log_id} to '{new_mood}'")
+
+
+# DELETE
+
+def delete_mood_log(log_id):
+    log = session.query(MoodLog).filter_by(id=log_id).first()
+    if not log:
+        print(f"Mood log with ID {log_id} not found.")
+        return
+    session.delete(log)
+    session.commit()
+    print(f"Deleted mood log ID {log_id}")
+
+
