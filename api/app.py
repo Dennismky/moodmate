1d1b65bb3a42973c51569771ca9b44f8957e41de
tinamanga <tinamanga@gmail.com>
2025-05-31 23:28:39 +0300

    updated readme

--- api/app.py
+++ api/app.py
@@ -1,3 +1,8 @@
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 from lib.db.models import User, MoodLog
