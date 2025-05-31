c568da97b7d4bfe2e593bb0b6a67fd342c063200
tinamanga <tinamanga@gmail.com>
2025-06-01 00:16:03 +0300

    checking

--- api/app.py
+++ api/app.py
@@ -109,5 +109,7 @@ def serve_react(path):
 
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
